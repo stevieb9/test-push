98403b3f2a63cb26f8bc126326c6ab25c293f7e6
Steve Bertrand <steveb@cpan.org>
2020-12-26 23:55:38 -0800

    Release 1.00 candidate

---
+++ git.pl
@@ -0,0 +1,38 @@
+use warnings;
+use strict;
+
+git_release('1.00');
+
+sub git_release {
+    my ($version) = @_;
+    die("git_release_commit() requires a version sent in") if ! defined $version;
+
+    print "Committing release candidate...\n";
+    
+    my $commit_exit = system("git commit -am 'Release $version candidate'");
+
+    die("Git commit failed... needs intervention...") if $commit_exit != 0;
+
+    print "Pushing release candidate to Github...\n";
+
+    my $push_exit = system("git push -am 'Release $version candidate'");
+
+    die("Git push failed... needs intervention...") if $push_exit != 0;
+
+    {
+        my $continue = 0;
+
+        local $SIG{INT} = sub { $continue = 1; };
+
+        my @spinner = qw(-- \ | / | \ / --);
+
+        while (! $continue) {
+            for (0..$#spinner) {
+                print $spinner[$_];
+            }
+        }
+
+        print "Done!\n";
+    }
+}
+
